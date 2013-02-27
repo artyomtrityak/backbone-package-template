2d642e45e62105ecbffca15ab0e5bf020b50ba60
f0t0n <t.34.oxygen@gmail.com>
2013-02-27 12:40:52 +0200

    Re-write app/server.js in CoffeeScript

--- app/server.js
+++ app/server.js
@@ -1,16 +1,23 @@
-/*global define: true, _: true, console:true, require: true, Backbone: true, $: true */
 
-define(['exports', 'sinon'], function (exports) {
-
-	exports.start = function () {
-		var server = sinon.fakeServer.create();
-		server.autoRespond = true;
-		server.autoRespondAfter = 50;
-
-		server.respondWith("GET", "/test",
-			[200, { "Content-Type": "application/json" },
-			'{ "id": 12, "comment": "Hey" }']
-		);
-	};
-
+define(['exports', 'sinon'], function(exports) {
+  exports.start = function() {
+    var requests, server;
+    server = sinon.fakeServer.create();
+    server.autoRespond = true;
+    server.autoRespondAfter = 50;
+    requests = [
+      {
+        method: 'GET',
+        route: '/test',
+        response: '{"id": 12, "comment": "Hey"}'
+      }
+    ];
+    return _.each(requests, function(req) {
+      return server.respondWith(req.method, req.route, [
+        200, {
+          'Content-Type': 'application/json'
+        }, req.response
+      ]);
+    });
+  };
 });
